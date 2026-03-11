61eb1effc8dad607f6d086b713341b6f45535d09
3lectronicbody <dominik.jarmocik@gmail.com>
2026-03-11 12:09:51 +0100

    Todo: Projects Window

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+# Database settings
 DATABASE_URL = "sqlite:///data.db"
+# Email settings
 EMAIL_ADDRESS = 'developeservice@yahoo.com'
 EMAIL_PASSWORD = 'hasloniemozebyc'
-
+# App settings
+DEBUG = True
